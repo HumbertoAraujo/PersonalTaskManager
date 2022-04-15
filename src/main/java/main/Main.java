248c307f00eb618256bf80e91bc2f37c1ea7f03a
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-04-15 19:04:40 -0300

    Interfaca gráfica tela principal ok

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package PersonalTaskManager;
+package main;
 
 import controller.ProjectController;
 import model.Project;
